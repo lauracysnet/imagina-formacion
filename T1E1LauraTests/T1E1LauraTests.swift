83b64416387765f33db1b9026c0b5f5ee319e20c
Laura <laura@MacMini-Laura.local>
2015-06-25 13:58:03 +0200

    Initial Commit

---
+++ T1E1LauraTests/T1E1LauraTests.swift
@@ -0,0 +1,36 @@
+//
+//  T1E1LauraTests.swift
+//  T1E1LauraTests
+//
+//  Created by Laura on 25/6/15.
+//  Copyright (c) 2015 ImaginaFormacion. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class T1E1LauraTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
