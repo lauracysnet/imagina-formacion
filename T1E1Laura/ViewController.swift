aae34a88a26a25feb1dbf07488fb4facde4c0e6d
Laura <laura@MacMini-Laura.local>
2015-06-25 14:08:31 +0200

    Commit remoto

--- T1E1Laura/ViewController.swift
+++ T1E1Laura/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         println("Hola Mundo!!")
+        println("------------")
     }
 
     override func didReceiveMemoryWarning() {
